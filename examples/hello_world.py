605a6e13b40ea9e9ad5b6b4c0169a6db0dc56f2a
alexander-cathis <alexander.cathis@utexas.edu>
2026-04-17 13:33:08 -0500

    Pause option between time steps

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 from gym_idsgame.envs import IdsGameEnv
 
-def attack_against_baseline_defense_env(render=False, seed=None):
+def attack_against_baseline_defense_env(render=False, seed=None, pause=False):
     versions = range(0,20)
     version = versions[0]
     env_name = "idsgame-minimal_defense-v" + str(version)
@@ -11,13 +11,15 @@ def attack_against_baseline_defense_env(render=False, seed=None):
     while not done:
         if render:
             env.render()
+        if pause:
+            input("Press Enter to continue...")
         attack_action = env.unwrapped.attacker_action_space.sample()
         defense_action = None
         a = (attack_action, defense_action)
         obs, reward, done, info = env.step(a)
 
 
-def attack_against_random_defense_env(seed=None):
+def attack_against_random_defense_env(render=False, seed=None, pause=False):
     versions = range(0,20)
     version = versions[0]
     env_name = "idsgame-random_defense-v" + str(version)
@@ -25,12 +27,16 @@ def attack_against_random_defense_env(seed=None):
     env.reset(seed=seed)
     done = False
     while not done:
+        if render:
+            env.render()
+        if pause:
+            input("Press Enter to continue...")
         attack_action = env.unwrapped.attacker_action_space.sample()
         defense_action = None
         a = (attack_action, defense_action)
         obs, reward, done, info = env.step(a)
 
-def defense_against_baseline_attack_env(seed=None):
+def defense_against_baseline_attack_env(render=False, seed=None, pause=False):
     versions = range(0,20)
     version = versions[0]
     env_name = "idsgame-maximal_attack-v" + str(version)
@@ -38,13 +44,17 @@ def defense_against_baseline_attack_env(seed=None):
     env.reset(seed=seed)
     done = False
     while not done:
+        if render:
+            env.render()
+        if pause:
+            input("Press Enter to continue...")
         attack_action = None
         defense_action = env.unwrapped.defender_action_space.sample()
         a = (attack_action, defense_action)
         obs, reward, done, info = env.step(a)
 
 
-def defense_against_random_attack_env(seed=None):
+def defense_against_random_attack_env(render=False, seed=None, pause=False):
     versions = range(0,20)
     version = versions[0]
     env_name = "idsgame-random_attack-v" + str(version)
@@ -52,12 +62,16 @@ def defense_against_random_attack_env(seed=None):
     env.reset(seed=seed)
     done = False
     while not done:
+        if render:
+            env.render()
+        if pause:
+            input("Press Enter to continue...")
         attack_action = None
         defense_action = env.unwrapped.defender_action_space.sample()
         a = (attack_action, defense_action)
         obs, reward, done, info = env.step(a)
 
-def two_agents_env(seed=None):
+def two_agents_env(render=False, seed=None, pause=False):
     versions = range(0,20)
     version = versions[0]
     env_name = "idsgame-v" + str(version)
@@ -65,19 +79,25 @@ def two_agents_env(seed=None):
     env.reset(seed=seed)
     done = False
     while not done:
+        if render:
+            env.render()
+        if pause:
+            input("Press Enter to continue...")
         attack_action = env.unwrapped.attacker_action_space.sample()
         defense_action = env.unwrapped.defender_action_space.sample()
         a = (attack_action, defense_action)
         obs, reward, done, info = env.step(a)
 
 def main():
-    # To enable rendering, set render=True in any of the environment simulations
-    # To specify a seed, pass the seed parameter
-    attack_against_baseline_defense_env(render=True, seed=0)
-    # attack_against_random_defense_env(seed=0)
-    # defense_against_baseline_attack_env(seed=0)
-    # defense_against_random_attack_env(seed=0)
-    # two_agents_env(seed=0)
+    render = True
+    seed = 0
+    pause = True
+
+    attack_against_baseline_defense_env(render=render, seed=seed, pause=pause)
+    # attack_against_random_defense_env(render=render, seed=seed, pause=pause)
+    # defense_against_baseline_attack_env(render=render, seed=seed, pause=pause)
+    # defense_against_random_attack_env(render=render, seed=seed, pause=pause)
+    # two_agents_env(render=render, seed=seed, pause=pause)
 
 if __name__ == '__main__':
     main()
